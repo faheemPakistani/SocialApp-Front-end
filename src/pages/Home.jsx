b0edc6a72833a2f1b6e33b1d85fc068e3ac010b2
faheemPakistani <69565922+faheemPakistani@users.noreply.github.com>
2021-07-04 14:14:29 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./home.css"
+import Topbar from "../../components/TopBar/Topbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
+export default function Home() {
+    return (
+    <>
+    <Topbar/>
+    <div className="homeContainer">
+    <Sidebar/> 
+    <Feed/>
+    <Rightbar/>
+    </div>
+    </>
+    )
+}
